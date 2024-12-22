c0f4639b388dc3fbbef1750553721a05346563c6
Полина <pobiryukova@icloud.com>
2024-12-22 21:20:33 +0300

    обновление с учетом новых вводных по словарю и ключу

--- final.py
+++ final.py
@@ -1,24 +1,17 @@
-information_about_note = []
+note = {}
 
-username = input('Введите имя пользователя: ')
-information_about_note.append(username)
+note ['username'] = input('Введите имя пользователя: ')
+note ['content'] = input('Введите описание заметки: ')
+note['status'] = input('Введите статус по работе с заметкой (например: в работе, в архиве, закрыта и проч.): ')
+note['created_date'] = input('Введите дату создания заметки в формате "день-месяц-год": ')
+note['issue_date'] = input('Введите дату истечения заметки (дедлайн) в формате "день-месяц-год":' )
 
-content = input('Введите описание заметки: ')
-information_about_note.append (content)
+note['titles'] = []
+for i in range(3):
+    title = input(f'Введите заголовок заметки {i + 1}: ')
+    note['titles'].append(title)
 
-title_1 = input('Введите первый заголовок заметки: ')
-title_2 = input('Введите второй заголовок заметки: ')
-title_3 = input('Введите третий заголовок заметки: ')
-titles = [title_1, title_2, title_3]
-information_about_note.append(titles)
-
-status = input('Введите статус по работе с заметкой (например: в работе, в архиве, закрыта и проч.): ')
-information_about_note.append(status)
-
-created_date = input('Введите дату создания заметки в формате "день-месяц-год": ')
-information_about_note.append(created_date)
-
-issue_date = input('Введите дату истечения заметки (дедлайн) в формате "день-месяц-год":' )
-information_about_note.append(issue_date)
-
-print(*information_about_note, sep='\n')
+print('\nСобранная информация о заметке:')
+print()
+for key, value in note.items():
+    print(f'{key.capitalize()}: {value}')
