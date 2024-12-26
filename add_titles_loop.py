39db3e0aa969d25055c2bb27e132e83588576cdb
Полина <pobiryukova@icloud.com>
2024-12-26 10:59:51 +0300

    каждый заголовок выводится с новой строки, доработала

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,28 +1,24 @@
 # Реализация цикла для добавления заголовков
 note = {}
 
-#note ['username'] = input('Введите имя пользователя: ')
-#note ['content'] = input('Введите описание заметки: ')
-#note['status'] = input('Введите статус по работе с заметкой (например: в работе, в архиве, закрыта и проч.): ')
-#note['created_date'] = input('Введите дату создания заметки в формате "день-месяц-год": ')
-#note['issue_date'] = input('Введите дату истечения заметки (дедлайн) в формате "день-месяц-год":' )
+note ['username'] = input('Введите имя пользователя: ')
+note ['content'] = input('Введите описание заметки: ')
+note['status'] = input('Введите статус по работе с заметкой (например: в работе, в архиве, закрыта и проч.): ')
+note['created_date'] = input('Введите дату создания заметки в формате "день-месяц-год": ')
+note['issue_date'] = input('Введите дату истечения заметки (дедлайн) в формате "день-месяц-год":' )
 
 note['titles'] = []
-title = input('Введите заголовок заметки: ')
-note['titles'].append(title)
 
 while True:
-    new_title = input('Желаете добавить еще один заголовок? (да/нет)\n').lower()
-    if new_title == 'нет':
-        continue_input = False
-        break
-    elif new_title == 'да':
-        title = input('Создание нового заголовка: ')
-        note['titles'].append(title)
+    title = input('Введите заголовок (или оставьте пустым для завершения): ')
+    if title != '':
+        note ['titles'].append(title)
     else:
-        print('Выберите да/нет')
-        new_title = input('Желаете добавить еще один заголовок? (да/нет)\n').lower()
+        break
 
 print('\nСобранная информация о заметке:')
 for key, value in note.items():
-    print(f'{key.capitalize()}: {value}')
+    if key == 'titles':
+        print(f"{key.capitalize()}:\n{'\n'.join(value)}")  # Выводим все заголовки с новой строки
+    else:
+        print(f"{key.capitalize()}: {value}")
